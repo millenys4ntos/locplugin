a57027761604a58f820fbce26508c0a4ebd58c7b
millenys4ntos <millenydossantos10.msc@gmail.com>
2021-05-14 06:38:03 +0900

    local

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,29 +1,5 @@
 // This is a JavaScript file
 
-$(document).on("click", "#alerta", function(){
-  navigator.notification.alert("Minha mensagem");
-});
-
-$(document).on("click", "#confirm", function(){
-  function confirma(buttonIndex){
-    if(buttonIndex == 1){
-      navigator.notification.alert("Escolheu a opção A");
-    }else{
-    navigator.notification.alert("Escolheu a opção B");
-    }
-
-  }
-  navigator.notification.confirm("Escolha A ou B",confirma,"Escolha:",['A', 'B']);
-});
-
-$(document).on("click", "#beep", function(){
-  navigator.notification.beep(3);
-});
-
-$(document).on("click", "#vibrar", function(){
-  navigator.vibrate(4000);
-});
-
 function mostraMapa(lat, long){
   L.mapquest.key = 'tGDUfJEaQLBevb0aM22oPgJBDIcSMoIa';
 
